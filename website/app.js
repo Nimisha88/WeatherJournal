ca75e6cac8633188058a12a2b4cba6bb3c6f34d1
Nimisha Viraj <nimishaviraj@Nimishas-MBP-2.home>
2021-07-16 03:39:44 -0400

    July 16th, 2021 - Added new styles and display Temp/Date/Feelings logic

--- website/app.js
+++ website/app.js
@@ -8,13 +8,23 @@ const zipCode = document.getElementById("zip");
 const userFeelings = document.getElementById("feelings");
 const submitButton = document.getElementById("generate");
 const date = document.getElementById("date");
-const temp = document.getElementById("temp");
+const temp = document.getElementById("temp").getElementsByTagName('span')[0];
 const content = document.getElementById("content");
 const userInput = document.querySelector(".ui");
 const journalEntry = document.querySelector(".wj-entry");
 const newEntryButton = document.getElementById("new-entry");
+const apiOtherData = document.querySelectorAll(".api-other-desc");
+const sunTimesData = document.querySelectorAll(".sun-desc");
+// const feelsLike = document.querySelector(".feels-like");
+// const humidity = document.querySelector(".humidity");
+// const wind = document.querySelector(".wind");
+
+
+const tempUnitHTML = '<sup><a id="inF" class="active-temp" href="#">°F</a>|<a id="inC" class="inactive-temp" href="#">°C</a></sup>';
 
 let displayData = [];
+let unitF;
+let unitC;
 
 async function postAsync(url='', data={}) {
   const response = await fetch(url, {
@@ -54,10 +64,62 @@ function UserEntry(zip, feelings) {
   this.feelings = feelings;
 }
 
+function toggleTempUnitClassesOnClick() {
+  unitF.classList.toggle('active-temp');
+  unitF.classList.toggle('inactive-temp');
+  unitC.classList.toggle('active-temp');
+  unitC.classList.toggle('inactive-temp');
+}
+
+function addTempUnitChangeListener() {
+  unitF.addEventListener('click', () => {
+    temp.childNodes[0].textContent = Math.round(displayData.main.temp);
+    apiOtherData[0].textContent = Math.round(displayData.main['feels_like']);
+    apiOtherData[2].textContent = `${Math.round(displayData.wind.speed)} mph`;
+    toggleTempUnitClassesOnClick();
+
+  });
+  unitC.addEventListener('click', () => {
+    temp.childNodes[0].textContent = Math.round(displayData.tempInC.temp);
+    apiOtherData[0].textContent = Math.round(displayData.tempInC['feels_like']);
+    apiOtherData[2].textContent = `${Math.round(displayData.wind.speed * 1.61)} kph`;
+    toggleTempUnitClassesOnClick();
+  });
+}
+
+function displayTemp() {
+  if (displayData.cod == 200) {
+
+    temp.innerHTML = `${Math.round(displayData.main.temp)}${tempUnitHTML}`;
+    apiOtherData[0].textContent = Math.round(displayData.main['feels_like']);
+    apiOtherData[1].textContent = `${displayData.main.humidity}%`;
+    apiOtherData[2].textContent = `${Math.round(displayData.wind.speed)} mph`;;
+
+
+    unitF = document.getElementById("inF");;
+    unitC = document.getElementById("inC");;
+
+    addTempUnitChangeListener();
+  }
+}
+
+function displaySunTimes() {
+  if (displayData.cod == 200) {
+    sunTimesData[0].textContent = (new Date(displayData.sys.sunrise * 1000)).toString().slice(16, 21);
+    sunTimesData[1].textContent = (new Date(displayData.sys.sunset * 1000)).toString().slice(16, 21);
+  }
+}
+
 function displayDate() {
   if (displayData.cod == 200) {
-    date.getElementsByTagName('h3')[0].textContent = (new Date(displayData.dt)).toGMTString();
+    date.getElementsByTagName('h3')[0].textContent = `${(new Date(displayData.dt*1000)).toString()}`;
     date.getElementsByTagName('p')[0].textContent = `${displayData.name}, ${displayData.zip}`;
+  }
+}
+
+function displayUserFeelings() {
+  if (displayData.cod == 200) {
+    content.getElementsByTagName('p')[0].textContent = displayData.feelings;
   }
 }
 
@@ -69,14 +131,17 @@ async function executePostRequest() {
     })
     .then((json)=>{
       console.log(displayData);
+      displayTemp();
+      displaySunTimes();
       displayDate();
+      displayUserFeelings();
     });
 }
 
 submitButton.addEventListener("click", ()=>{
   executePostRequest();
   userInput.style.display = 'none';
-  journalEntry.style.display = 'inline';
+  journalEntry.style.display = 'block';
 });
 
 newEntryButton.addEventListener("click", ()=>{
